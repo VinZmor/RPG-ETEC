c1809e6d91e0adffa6429fa4e592e66a20560bf6
3º MTEC - Desenv. de Sistemas <ds-mtec-3@servidor.acad>
2025-03-28 14:05:28 -0300

    update

---
+++ controllers/routes.py
@@ -0,0 +1,9 @@
+from flask import render_template, request, redirect, url_for
+
+def init_app(app):
+  
+    @app.route('/')
+   
+    def home():
+        return render_template('telaCadastro.html')
+    
