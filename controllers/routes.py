5d6cbb96f2f5404ef5ad79523b044b1082a1a155
unknown <viniciuszezilia0@gmail.com>
2025-05-31 15:22:37 -0300

    update

--- controllers/routes.py
+++ controllers/routes.py
@@ -1,10 +1,13 @@
-from flask import render_template, request, redirect, url_for
+from flask import render_template, request
+from models.database import Bestiario, db
 
 def init_app(app):
-  
-    @app.route('/')
-   
-    def home():
-        return render_template('Wiki.html')
-    
-    
+    @app.route('/wikiBestiario', methods=['GET'])
+    @app.route('/wikiBestiario/<int:id>', methods=['GET'])
+    def home(id=None):
+        if id:
+            bestiariowiki = [Bestiario.query.get(id)]
+        else:
+            bestiariowiki = Bestiario.query.all()
+
+        return render_template('Wiki.html', bestiariowiki=bestiariowiki)
--- models/database.py
+++ models/database.py
@@ -2,14 +2,25 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-# Criando o Model (classe)
-class Lista(db.Model):
+class Bestiario(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    topico = db.Column(db.String(150))
-    conteudo = db.Column(db.Integer)
-    
-    # Método construtor da classe
-    def __init__(self, conteudo, topico):
-        self.conteudo = conteudo
-        self.topico = topico
-        
+    nome = db.Column(db.String(150))
+    descricao = db.Column(db.String(150))
+    nd = db.Column(db.String(150))
+    atributos = db.Column(db.Integer) 
+    pericias = db.Column(db.String(150))
+    velocidade = db.Column(db.Integer)
+    vida = db.Column(db.Integer)
+    mental = db.Column(db.Integer)
+    energia = db.Column(db.Integer)
+
+    def __init__(self, nome, descricao, nd, atributos, pericias, velocidade, vida, mental, energia):
+        self.nome = nome
+        self.descricao = descricao
+        self.nd = nd
+        self.atributos = atributos
+        self.pericias = pericias
+        self.velocidade = velocidade
+        self.vida = vida
+        self.mental = mental
+        self.energia = energia
